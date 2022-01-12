8d76ec9986ff8adfed9df55b3a47cdf17f82bd65
Supersagee <williamdsage@gmail.com>
2022-01-11 21:36:58 -0600

    initial commit

---
+++ OperatorExerciseConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorExerciseConsole
+{
+    internal class Program
+    {
+        public static double AreaOfCircle(double r)
+        {
+            var circleArea = Math.PI * (r * r);
+
+            return circleArea;
+        }
+
+        static void Main(string[] args)
+        {
+
+            var a = 17;
+            var b = 4;
+
+            var quotient = a / b; 
+            var remainder = a % b;
+
+            Console.WriteLine($"17 divided by 4 equals {quotient} with a remainder of {remainder}");
+
+            Console.WriteLine("Give a number for the radius of a circle");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of your circle is {AreaOfCircle(radius)}");
+
+        }
+    }
+}
